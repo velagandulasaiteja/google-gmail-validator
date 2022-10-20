55d1957fb4398299fd7f2e573f48202929a51e3a
saivelagandula <saivelagandula@rapidinnovation.dev>
2022-10-20 22:10:10 +0530

    committed 1.0.1 code

---
+++ index.js
@@ -0,0 +1,24 @@
+exports.validateGmail = function (param) {
+  let gmail = `${param}`;
+  let trimmedGmail = gmail.trim().replace(" ", "");
+  let reg = /[a-zA-Z0-9]/;
+  let reg1 = /[a-z]+/g;
+  let reg2 = /[^a-zA-Z0-9\.]/g;
+  if (gmail[0].match(reg) === null) {
+    return false;
+  } else if (gmail.length !== trimmedGmail.length) {
+    return false;
+  } else if (gmail.slice(-10) === "@gmail.com") {
+    let reverseGmail = gmail.split("").reverse().join("");
+    const username = reverseGmail.slice(10).split("").reverse().join("");
+    if (!(username.length >= 6 && username.length <= 30)) {
+      return false;
+    } else if (username.length >= 8 && username.match(reg1) === null) {
+      return false;
+    } else if (username.match(reg2) !== null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
